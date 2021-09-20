8bdd7bcc8d04c65e138ad3113950167a41a6aba9
Jelena Runje <jrunje00@gmail.com>
2021-09-20 15:22:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/drinktalk/LoginScreen.java
@@ -0,0 +1,19 @@
+package com.example.drinktalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+    }
+}
---
+++ app/src/main/java/com/example/drinktalk/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.drinktalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginScreen.class));
+            }
+        }, 3000);
+    }
+}
