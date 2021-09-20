9fe46c07397ad5da5aeb4c7adac760317ec3e3b9
Jelena Runje <jrunje00@gmail.com>
2021-09-20 19:08:42 +0200

    nesto

--- app/src/main/java/com/example/drinktalk/LoginScreen.java
+++ app/src/main/java/com/example/drinktalk/LoginScreen.java
@@ -2,18 +2,29 @@ package com.example.drinktalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 
 public class LoginScreen extends AppCompatActivity {
+
+    ImageView imgLogo;
+    ImageButton btnPrijaviSe;
+    EditText etLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+
+        imgLogo=findViewById(R.id.imgLogo);
+        btnPrijaviSe=findViewById(R.id.btn_prijavi_se);
+        etLogin=findViewById(R.id.et_login);
+
 
     }
 }
--- app/src/main/java/com/example/drinktalk/SplashScreen.java
+++ app/src/main/java/com/example/drinktalk/SplashScreen.java
@@ -1,10 +1,8 @@
 package com.example.drinktalk;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
