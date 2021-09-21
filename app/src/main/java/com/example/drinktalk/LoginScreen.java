253f91e700030c1c37b6779118555de0bae626a5
Jelena Runje <jrunje00@gmail.com>
2021-09-21 12:31:21 +0200

    nesto

--- app/src/main/java/com/example/drinktalk/LoginScreen.java
+++ app/src/main/java/com/example/drinktalk/LoginScreen.java
@@ -3,7 +3,9 @@ package com.example.drinktalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -21,11 +23,24 @@ public class LoginScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-
         imgLogo=findViewById(R.id.imgLogo);
         btnPrijaviSe=findViewById(R.id.btn_prijavi_se);
         etLogin=findViewById(R.id.et_login);
 
+        btnPrijaviSe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId()==R.id.btn_prijavi_se){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentHellou()).commit();
+
+                    // e ovde sad triba ono iz login.screen.xml NEKAKO SAKRIT setvisible stogod// JA sam podilila u dva relativa ,
+                    // i fragment container je ono di se triba minjat , jer ovo sve ostaje isto -mozda bi bilo bolje minjat cili
+                    //screen, nezz??! ovako je uglavnom sve pomisano :D
+                    //al aj funkcionira ovo s fragmentima, samo pazi , na iducem se zove fragment iz fragmenta
+                }
+            }
+
+        });
 
     }
 }
